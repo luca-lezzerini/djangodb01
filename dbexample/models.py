b3fa59946f0431592a60c3b72985bf333af5064b
ceda-dev-01 <l.lezzerini@coopceda.it>
2024-11-19 19:34:11 +0100

    migrations have been generated for course and student

--- dbexample/models.py
+++ dbexample/models.py
@@ -6,3 +6,9 @@ class Student(models.Model):
     firstname = models.CharField(max_length=255)
     lastname = models.CharField(max_length=255)
     studentID = models.IntegerField()
+
+
+class Course(models.Model):
+    title=models.CharField(max_length=255)
+    code=models.CharField(max_length=10)
+    credits = models.IntegerField()
