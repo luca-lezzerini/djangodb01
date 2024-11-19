938b477f3bf685620017e37eff1791ae53ddc115
ceda-dev-01 <l.lezzerini@coopceda.it>
2024-11-19 19:20:45 +0100

    Added model and view

--- dbexample/views.py
+++ dbexample/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from dbexample.models import Student
+
+
 # Create your views here.
+def addstudent(request):
+    s = Student(firstname="Ledio",lastname="Hoxha",studentID=1000)
+    s.save()
