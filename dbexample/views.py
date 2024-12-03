68dc2767dbb6d7bc05182c7be9f2ce8d961ffb5a
ceda-dev-01 <l.lezzerini@coopceda.it>
2024-12-03 19:07:32 +0100

    Added  template to the previous code

--- dbexample/views.py
+++ dbexample/views.py
@@ -1,11 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
 
 from dbexample.models import Student
 
 
 # Create your views here.
 def addstudent(request):
+    template = loader.get_template('mytemplate.html')
     s = Student(firstname="Ledio",lastname="Hoxha",studentID=1000)
     s.save()
     res = Student.objects.all().values()
@@ -14,4 +16,7 @@ def addstudent(request):
     todelete.delete()
     res2 = Student.objects.all().values()
     print(res2)
-    return HttpResponse(res2)
+    context = {
+        'students': res2,
+    }
+    return HttpResponse(template.render(context, request))
